6a022d4eb4aa3bb3af78c9a392fd1cefc82f5575
jonakapaj <jonakapajj@gmail.com>
2024-05-08 22:52:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from './client/auth/Register';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import GuardedRoute from './GuardedRoute';
+import Login from './client/auth/Login';
+import {UserProvider} from "/Users/jonakapaj/recipiepublisher/recipie/src/user/UserContext.js";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/homepage" element={<GuardedRoute>
+            <Home/>
+        </GuardedRoute>}/>
+    </Routes>
+</BrowserRouter>
+</UserProvider>
+
   );
 }
 
